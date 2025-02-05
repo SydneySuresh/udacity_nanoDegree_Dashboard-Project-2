df35920fa695ae9fd9ac866bec963587a7b62f14
Bader Almutlaq <badr11almutlaq@gmail.com>
2025-02-05 17:51:24 +0300

    implementing the mixin

--- python-package/employee_events/sql_execution.py
+++ python-package/employee_events/sql_execution.py
@@ -6,28 +6,37 @@ import pandas as pd
 # Using pathlib, create a `db_path` variable
 # that points to the absolute path for the `employee_events.db` file
 #### YOUR CODE HERE
+db_path = Path(__file__).parent / "employee_events.db"
+db_path.resolve()
 
 
 # OPTION 1: MIXIN
 # Define a class called `QueryMixin`
 class QueryMixin:
-    
+
     # Define a method named `pandas_query`
     # that receives an sql query as a string
     # and returns the query's result
     # as a pandas dataframe
-    #### YOUR CODE HERE
+    def pandas_query(self, sql_query: str) -> pd.DataFrame:
+        """Executes an SQL query and returns the result as a Pandas DataFrame."""
+        with connect(db_path) as connection:
+            return pd.read_sql(sql_query, connection)
 
     # Define a method named `query`
     # that receives an sql_query as a string
     # and returns the query's result as
     # a list of tuples. (You will need
     # to use an sqlite3 cursor)
-    #### YOUR CODE HERE
-    
+    def query(self, sql_query: str) -> list:
+        """Executes an SQL query and returns the result as a list of tuples."""
+        with connect(db_path) as connection:
+            cursor = connection.cursor()
+            result = cursor.execute(sql_query).fetchall()
+        return result
 
- 
- # Leave this code unchanged
+
+# Leave this code unchanged
 def query(func):
     """
     Decorator that runs a standard sql execution
@@ -42,5 +51,5 @@ def query(func):
         result = cursor.execute(query_string).fetchall()
         connection.close()
         return result
-    
+
     return run_query
