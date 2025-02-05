7852b970c9e1e0cd7675dd8e30bd88b0e9f2e490
Bader Almutlaq <badr11almutlaq@gmail.com>
2025-02-05 18:20:51 +0300

    employee class implemented

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,49 +1,55 @@
 # Import the QueryBase class
-#### YOUR CODE HERE
+from query_base import QueryBase
 
-# Import dependencies needed for sql execution
-# from the `sql_execution` module
-#### YOUR CODE HERE
 
 # Define a subclass of QueryBase
 # called Employee
-#### YOUR CODE HERE
+class Employee(QueryBase):
 
     # Set the class attribute `name`
     # to the string "employee"
-    #### YOUR CODE HERE
-
+    name = "employee"
 
     # Define a method called `names`
     # that receives no arguments
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
-        
+    def names(self):
         # Query 3
         # Write an SQL query
-        # that selects two columns 
+        # that selects two columns
         # 1. The employee's full name
         # 2. The employee's id
         # This query should return the data
         # for all employees in the database
-        #### YOUR CODE HERE
-    
+
+        query_str = """SELECT
+                        CONCAT (first_name, " ", last_name) AS full_name,
+                        employee_id
+                    FROM
+                        employee"""
+        return self.query(query_str)
 
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
-        
+
+    def username(self, id):
+
         # Query 4
         # Write an SQL query
         # that selects an employees full name
         # Use f-string formatting and a WHERE filter
         # to only return the full name of the employee
         # with an id equal to the id argument
-        #### YOUR CODE HERE
-
+        query_str = f"""SELECT
+                        CONCAT (first_name, " ", last_name) AS full_name,
+                        employee_id
+                    FROM
+                        employee
+                    WHERE employee_id = {id}"""
+        return self.query(query_str)
 
     # Below is method with an SQL query
     # This SQL query generates the data needed for
