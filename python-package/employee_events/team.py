f9857aee2838b5d91987a5b35b576f193bdf2b34
Bader Almutlaq <badr11almutlaq@gmail.com>
2025-02-05 18:27:14 +0300

    team class implemented

--- python-package/employee_events/team.py
+++ python-package/employee_events/team.py
@@ -1,46 +1,52 @@
 # Import the QueryBase class
-# YOUR CODE HERE
+from query_base import QueryBase
 
-# Import dependencies for sql execution
-#### YOUR CODE HERE
 
 # Create a subclass of QueryBase
 # called  `Team`
 #### YOUR CODE HERE
-
+class Team(QueryBase):
     # Set the class attribute `name`
     # to the string "team"
-    #### YOUR CODE HERE
-
+    name = "team"
 
     # Define a `names` method
     # that receives no arguments
     # This method should return
     # a list of tuples from an sql execution
     #### YOUR CODE HERE
-        
+    def names(self):
         # Query 5
         # Write an SQL query that selects
         # the team_name and team_id columns
         # from the team table for all teams
         # in the database
         #### YOUR CODE HERE
-    
+        query_str = f"""SELECT
+                        team_name,
+                        team_id
+                    FROM
+                        {self.name}"""
+        return self.query(query_str)
 
     # Define a `username` method
     # that receives an ID argument
     # This method should return
     # a list of tuples from an sql execution
     #### YOUR CODE HERE
-
+    def username(self, id):
         # Query 6
         # Write an SQL query
         # that selects the team_name column
         # Use f-string formatting and a WHERE filter
         # to only return the team name related to
         # the ID argument
-        #### YOUR CODE HERE
-
+        query_str = f"""SELECT
+                        team_name
+                    FROM
+                        {self.name}
+                    WHERE {self.name}_id = {id}"""
+        return self.query(query_str)
 
     # Below is method with an SQL query
     # This SQL query generates the data needed for
