ba01f6e039b2fd48bb44a38cd659a9de0bf4c68c
Rapheal Ajiboye <raphoooo95@gmail.com>
2025-04-11 17:17:25 +0100

    add: dashboard task completion

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,25 +1,27 @@
 # Import the QueryBase class
 #### YOUR CODE HERE
+from employee_events.query_base import QueryBase
+
 
 # Import dependencies needed for sql execution
 # from the `sql_execution` module
 #### YOUR CODE HERE
-
+from employee_events.sql_execution import QueryMixin
 # Define a subclass of QueryBase
 # called Employee
 #### YOUR CODE HERE
-
+class Employee(QueryBase):
     # Set the class attribute `name`
     # to the string "employee"
     #### YOUR CODE HERE
-
+    name = "employee"
 
     # Define a method called `names`
     # that receives no arguments
     # This method should return a list of tuples
     # from an sql execution
     #### YOUR CODE HERE
-        
+    def names(self):
         # Query 3
         # Write an SQL query
         # that selects two columns 
@@ -27,15 +29,21 @@
         # 2. The employee's id
         # This query should return the data
         # for all employees in the database
-        #### YOUR CODE HERE
-    
+        # YOUR CODE HERE
+        query = """
+                SELECT first_name || ' ' || last_name, employee_id 
+                FROM employee
+                """
+
+        # Fetch the data from the model using the query
+        return QueryMixin.query(sql_query=query)
 
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
     #### YOUR CODE HERE
-        
+    def username(self, id):
         # Query 4
         # Write an SQL query
         # that selects an employees full name
@@ -43,7 +51,12 @@
         # to only return the full name of the employee
         # with an id equal to the id argument
         #### YOUR CODE HERE
-
+        query = f"""
+                SELECT first_name || ' ' || last_name 
+                FROM employee
+                WHERE employee_id = {id}
+                """
+        return QueryMixin.query(sql_query=query)
 
     # Below is method with an SQL query
     # This SQL query generates the data needed for
@@ -54,8 +67,7 @@
     # the sql query
     #### YOUR CODE HERE
     def model_data(self, id):
-
-        return f"""
+        query = f"""
                     SELECT SUM(positive_events) positive_events
                          , SUM(negative_events) negative_events
                     FROM {self.name}
@@ -63,3 +75,5 @@
                         USING({self.name}_id)
                     WHERE {self.name}.{self.name}_id = {id}
                 """
+
+        return QueryMixin.pandas_query(sql_query=query)
--- python-package/employee_events/sql_execution.py
+++ python-package/employee_events/sql_execution.py
@@ -6,17 +6,23 @@ import pandas as pd
 # Using pathlib, create a `db_path` variable
 # that points to the absolute path for the `employee_events.db` file
 #### YOUR CODE HERE
+db_path = Path(__file__).parent/"employee_events.db"
 
 
 # OPTION 1: MIXIN
 # Define a class called `QueryMixin`
 class QueryMixin:
-    
     # Define a method named `pandas_query`
     # that receives an sql query as a string
     # and returns the query's result
     # as a pandas dataframe
     #### YOUR CODE HERE
+    @staticmethod
+    def pandas_query(sql_query: str) -> pd.DataFrame:
+        """Executes a query and returns a pandas DataFrame"""
+        with connect(db_path) as conn:
+            df = pd.read_sql_query(sql_query, conn)
+        return df
 
     # Define a method named `query`
     # that receives an sql_query as a string
@@ -24,6 +30,14 @@ class QueryMixin:
     # a list of tuples. (You will need
     # to use an sqlite3 cursor)
     #### YOUR CODE HERE
+    @staticmethod
+    def query(sql_query: str) -> list:
+        """Executes a query and returns a list of tuples"""
+        with connect(db_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute(sql_query)
+            results = cursor.fetchall()
+        return results
     
 
  
