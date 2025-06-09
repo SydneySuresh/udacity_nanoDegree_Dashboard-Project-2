19565e462529e0fa4a6aa99e3f99c6319c6cc0c2
SydneySuresh <suresh_kannan_na@hotmail.com>
2025-06-09 15:57:43 +1000

    updated fixes
    updated fixes

--- report/dashboard.py
+++ report/dashboard.py
@@ -120,7 +120,7 @@ class LineChart(MatplotlibViz):
         # Reference the base_components/matplotlib_viz file
         # to inspect the supported keyword arguments
         # YOUR CODE HERE
-        self.set_axis_styling(ax, border_color='black', font_color='black')
+        self.set_axis_styling(ax, bordercolor='black', fontcolor='black')
         # Set title and labels for x and y axis
         # YOUR CODE HERE
         ax.set_title('Cumulative Performance Events')
@@ -183,7 +183,7 @@ class BarChart(MatplotlibViz):
         # to the `.set_axis_styling`
         # method
         # YOUR CODE HERE
-        self.set_axis_styling(ax, border_color='black', font_color='black')
+        self.set_axis_styling(ax, bordercolor='black', fontcolor='black')
         return fig
 
 
